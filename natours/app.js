c4f1cd99ff4366b15467065333072f13f9e2947e
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-07-01 12:35:18 +0800

    Controllers created

--- natours/app.js
+++ natours/app.js
@@ -12,42 +12,13 @@ const userRouter = require('./routes/userRoutes')
 app.use(express.json())
 
 
-//==========ROUTING========
-
-
-
-
-
-
-
-
-
-
- // = mounting router
-//We will pass this as a middleware
-
-//express.Router() is a function on express, when you console.log(tourRouter), you will get this
-/**
- * function router(req, res, next) {
-    router.handle(req, res, next);
-  }
-  This tells you that this is a middleware
-  When you console.log(tourRouter) in the route handler you will see that there is a method call route that accepts the path and method of the route
-  So after defining this route as tourRouter.route('/').get(getAllTours).post(createTour); then we pass it to app.use() as a middleware
-  //Middleware takes in a function, in our case it first check if the user is on the right path as '/api/v1/tours' then it will execute the function passed to
- */ 
-//console.log('tourRouter', tourRouter.route)
 
 //=====PASSING THE ROUTE AS A MIDDLEWARE====
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+module.exports = app;
+
 
  
 
-
-///////////// SERVER //////////
-const port = 3000
-app.listen(port, () => {
- console.log(`App is runing on port ${port}`)
-})
---
+++ natours/server.js
@@ -0,0 +1,9 @@
+
+const app = require('./app')
+
+///////////// SERVER //////////
+const port = 3000
+app.listen(port, () => {
+ console.log(`App is runing on port ${port}`)
+})
+
