d3183b72d729bb17fd158ab6100dd8acf6336a45
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-07-02 13:22:43 +0800

    Database connection

--- natours/server.js
+++ natours/server.js
@@ -1,15 +1,47 @@
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const mongoose = require('mongoose')
 const app = require('./app')
 
-
-
-//====ENVIRONMENT VARIABLE
-//This helps us to hide our sensitve data
-//After creating the env files you need a package to connect the files to your application, this is called dotenv
-
 dotenv.config({path: './config.env'}) //This will read the files we defined and saved them into node js environemnt variable so that we can retrieve them at anytime
+//console.log(process.env) //This runs all our environemnt varriables
 
-console.log(process.env) //This runs all our environemnt varriables
+
+//DB Connection
+
+mongoose.connect(process.env.DATABASE_LOCAL, {
+  useNewUrlParser: true,
+  useCreateIndex:true,
+  useFindAndModify:false
+}).then(con => {
+  //  console.log(con.connection)
+   console.log('DB, Connected')
+})
+
+
+//CREATING DB
+
+//Schema is a blueprint that's used to create document/object
+//We create model out of a schema
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'Must have a price']
+  }
+});
+
+//Creating model
+const Tour = mongoose.model('Tour', tourSchema)
+
 
 
 ///////////// SERVER //////////
